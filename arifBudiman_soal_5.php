3836a571548c5e9b3b4648d812461ef829507785
abudriman <abudriman@gmail.com>
2021-05-03 15:55:08 +0700

    finish*

--- arifBudiman_soal_5.php
+++ arifBudiman_soal_5.php
@@ -4,7 +4,7 @@ $menu = ["Makanan", "Minuman", "Parsel"];
 foreach($menu as $v){
     echo $v."\n";
     echo "1. Menu $v ke 1\n";
-    echo "1. Menu $v ke 2\n";
-    echo "1. Menu $v ke 3\n";
+    echo "2. Menu $v ke 2\n";
+    echo "3. Menu $v ke 3\n";
 }
 ?>
